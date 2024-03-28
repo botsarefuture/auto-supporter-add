112a38c8dd2ae072f677c690b8681af12bd62ad9
Verso <vuoreol@pm.me>
2024-03-28 14:59:08 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,35 @@
 import requests
 import csv
-
+import requests
+import csv
 import re
+from jinja2 import Template
+import os
+from github import Github
+import re
+
 
 def build_html_content(supporters):
     # Load Jinja2 template from file
-    with open("template.html", "r", encoding="utf-8") as template_file:
+    with open("template_en.html", "r", encoding="utf-8") as template_file:
         template_content = template_file.read()
 
     # Create Jinja2 template object
     template = Template(template_content)
 
     # Render the template with supporter data
-    rendered_html = template.render(supporters=supporters)
+    rendered_en = template.render(supporters=supporters)
 
-    return rendered_html
+    with open("template_fi.html", "r", encoding="utf-8") as template_file:
+        template_content = template_file.read()
+
+    # Create Jinja2 template object
+    template = Template(template_content)
+
+    # Render the template with supporter data
+    rendered_fi = template.render(supporters=supporters)
+    
+    return rendered_fi, rendered_en
 
 
 def validate_domain(domain):
@@ -84,6 +99,22 @@ def build_html_row(data):
 
     return html
 
+def upload_to_github(html_en, html_fi):
+    # Authenticate with GitHub using personal access token
+    g = Github("")
+
+    # Get the repository where you want to upload the file
+    repo = g.get_repo("botsarefuture/mielenterveyskaikille.fi")
+
+    # Create a new file in the repository
+    file_name = "supporters.html"
+    commit_message = "Add rendered HTML file"
+    repo.create_file(file_name, commit_message, html_fi)
+
+    file_name = "en/supporters.html"
+    commit_message = "Add rendered HTML file"
+    repo.create_file(file_name, commit_message, html_en)
+
 if __name__ == "__main__":
     supporters = fetch_supporters()
     whole_html = "<ul>\n"
@@ -93,5 +124,6 @@ if __name__ == "__main__":
 
     whole_html += "</ul>"
 
-    print(whole_html)
+    content_fi, content_en = build_html_content(whole_html)
 
+    upload_to_github(content_en, content_fi)
